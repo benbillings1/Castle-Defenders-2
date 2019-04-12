f29986b24bbefc45b41faab5cf14798981ed62f3
benbilli <benbilli@iu.edu>
2019-04-11 21:21:59 -0400

    Created health bar that can be used for player and enemies

--- Castle Defenders/Castle Defenders/Assets/Scripts/FireDragon.cs
+++ Castle Defenders/Castle Defenders/Assets/Scripts/FireDragon.cs
@@ -2,11 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FireDragon : MonoBehaviour
 {
     public int fireDamage = 10;
     public int health = 100;
+    private EnemyDragon enemyDragon;
 
 
+    void Start()
+    {
+        GameObject enemyDragonObject = GameObject.FindWithTag("Enemy");
+
+        if (enemyDragonObject != null)
+        {
+            enemyDragon = enemyDragonObject.GetComponent<EnemyDragon>();
+        }
+        if (enemyDragonObject == null)
+        {
+            Debug.Log("Cannot find 'EnemyDragon' script");
+        }
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        transform.LookAt(other.transform);
+        enemyDragon.TakeDamage(fireDamage);
+
+    }
+    
+        
+   
     
 }
